67697631397a89dfbfd78087a4ebf137e1885959
kimi <kimilenerand972@gmail.com>
2026-03-05 10:55:20 -0400

    màj

---
+++ listes-electorales-martinique/scripts/init-projet.sh
@@ -0,0 +1,5 @@
+﻿$projectName = $args[0]
+if (-not $projectName) { Write-Host "Erreur : nom manquant" -ForegroundColor Red; exit }
+New-Item -ItemType Directory -Path "$projectName/src", "$projectName/tests", "$projectName/docs" -Force
+New-Item -ItemType File -Path "$projectName/README.md", "$projectName/.gitignore" -Force
+Write-Host "Projet '$projectName' créé !" -ForegroundColor Green
